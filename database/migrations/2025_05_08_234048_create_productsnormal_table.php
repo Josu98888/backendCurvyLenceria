e18f51aee905262afc4219402e770c9b5b29edc1
Josué Aquino <aquinojosue889@gmail.com>
2025-05-08 21:04:14 -0300

    Se creó la tabla productsnormal

---
+++ database/migrations/2025_05_08_234048_create_productsnormal_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productsnormal', function (Blueprint $table) {
+            $table->unsignedBigInteger('id');                 // misma definición que el id de products
+            $table->primary('id');                            // hacer que este ID sea la PK
+            $table->foreign('id')->references('id')->on('products')->onDelete('cascade');
+            $table->decimal('price', 8, 2);
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productsnormal');
+    }
+};
