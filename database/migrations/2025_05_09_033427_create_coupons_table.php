d9bb3f33acca3070e95dcbf943b63e400516c190
Josué Aquino <aquinojosue889@gmail.com>
2025-05-09 00:50:21 -0300

    Se creó la tabla coupons

---
+++ database/migrations/2025_05_09_033427_create_coupons_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->decimal('discount_percentage', 5, 2);
+            $table->date('end_date');
+            $table->decimal('amount_min', 10, 2);
+            $table->unsignedInteger('use_max');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coupons');
+    }
+};
