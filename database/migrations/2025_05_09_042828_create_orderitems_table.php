bfd6d69ef29c226035e2d11f1cc2ff4cbc260c3c
Josué Aquino <aquinojosue889@gmail.com>
2025-05-09 01:31:52 -0300

    Se creó la tabla orderitems

---
+++ database/migrations/2025_05_09_042828_create_orderitems_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orderitems', function (Blueprint $table) {
+            $table->primary(['order_id', 'product_id']);
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->decimal('price', 10, 2);
+            $table->unsignedInteger('quantity');
+            $table->decimal('subtotal', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orderitems');
+    }
+};
