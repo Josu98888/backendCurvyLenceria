0098d7ab64d39d127cdfc9c8893acb68608a3f35
Josué Aquino <aquinojosue889@gmail.com>
2025-05-11 23:52:20 -0300

    Se creó el modelo de la tabla addresses

---
+++ app/Models/Address.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    protected $table = 'addresses';
+
+    protected $fillable = [
+        'user_id',
+        'street',
+        'number',
+        'postal_code',
+        'city',
+        'province',
+        'country',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+}
