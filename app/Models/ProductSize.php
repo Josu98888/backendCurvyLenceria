fd2317d223f9aa97e3c35fbbb147e1b3b85e59f4
Josué Aquino <aquinojosue889@gmail.com>
2025-05-11 23:44:46 -0300

    Se creó el modelo de la tabla productssize

---
+++ app/Models/ProductSize.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductSize extends Model
+{
+    protected $table = 'productssize';
+
+    protected $fillable = [
+        'id',
+        'size_id',
+        'price',
+        'stock',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+}
