a422af83ff89335e24a465ef8a40da7fc73a0aa7
Josué Aquino <aquinojosue889@gmail.com>
2025-05-11 23:49:37 -0300

    Se creó el modelo de la tabla images

---
+++ app/Models/Image.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    protected $fillable = [
+        'url',
+        'is_primary',
+        'product_id',
+    ];
+
+    protected $hiden = [
+        'created_at',
+        'updated_at',
+    ];
+}
